d0260bb8364ff144d7d2d67671cdff8fc986447b
Sharjeel Khan <sharjeelkhan@google.com>
2025-12-31 12:30:00 +0000

    [C++23] Fixes for C++23 issues
    C++23 removed certain transitive includes so you need to add C++ headers
    directly to utilize their functions and classes. We add cstdlib header
    for abort.
    
    In addition, C++23 changed unique_ptr to a constexpr so you need the
    type to be completely defined before you pass it to unique_ptr. We
    cannot fix swiftshader's LLVM 16 so we just pin mesa_gallium to C++17.
    We are pinning to C++17 to be consistent with Meson options.
    
    Errors:
    external/mesa3d/src/gfxstream/guest/vulkan_enc/goldfish_vk_counting_guest.cpp:9505:13:
    error: use of undeclared identifier 'abort'
     9505 |             abort();
    prebuilts/clang/host/linux-x86/clang-r584948/include/c++/v1/__memory/unique_ptr.h:90:19:
    error: invalid application of 'sizeof' to an incomplete type
    'llvm::APFloat'
       90 |     static_assert(sizeof(_Up) >= 0, "cannot delete an incomplete
    type");
    
    Flag: EXEMPT PURE_REFACTOR
    Bug: 429048222
    Test: m mesa_gallium vulkan.ranchu
    Change-Id: I3aa9a4a3067908c362e6dd3b77573f4432c2c04d

--- src/gfxstream/codegen/scripts/cerealgenerator.py
+++ src/gfxstream/codegen/scripts/cerealgenerator.py
@@ -518,6 +518,10 @@ using DlSymFunc = void* (void*, const char*);
 #include "goldfish_vk_private_defs.h"
 """
 
+        countingIncludeGuest = """
+#include <cstdlib>
+"""
+
         dispatchImplIncludes = """
 #include <stdio.h>
 #include <stdlib.h>
@@ -627,7 +631,7 @@ class BumpPool;
                                        extraImpl=commonCerealImplIncludesGuest + deepcopyInclude)
             self.addGuestEncoderModule("goldfish_vk_counting_guest",
                                        extraHeader=countingIncludes,
-                                       extraImpl=commonCerealImplIncludesGuest)
+                                       extraImpl=commonCerealImplIncludesGuest + countingIncludeGuest)
             self.addGuestEncoderModule("goldfish_vk_transform_guest",
                                        extraHeader=commonCerealIncludesGuest + transformIncludeGuest,
                                        extraImpl=commonCerealImplIncludesGuest + transformImplIncludeGuest)
